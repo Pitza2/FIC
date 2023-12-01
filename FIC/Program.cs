78dc95e88c1a9f5c59af3dfdf2577ca4ac48e8f7
Lica Andrei <ovidiu.lica@student.upt.ro>
2023-12-01 23:04:43 +0200

    processor simulator with arithmetic and procedures

---
+++ FIC/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using FIC;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ControlUnit cu = new ControlUnit(null);
+        ArithmeticLogicUnit alu = new ArithmeticLogicUnit(null);
+        RiscProcessor processor = new RiscProcessor(cu,alu);
+        
+        processor.load();
+        processor.run();
+    }
+}
---
+++ FIC/RiscProcessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIC.Interfaces;
+
+namespace FIC
+{
+    internal class RiscProcessor : IProcessor
+    {
+        
+        static readonly string[] reservedKeywords = { "INP" ,"OUT", "ADD", "SUB", "MUL", "DIV", "BRA", "CMP", "BEQ", "HLT", "RET", "JMS" };
+        Dictionary<string, short> labelLine=new Dictionary<string, short>();
+        Dictionary<string, short> registers;
+        List<short> mainMemory;
+        List<string> instructions;
+        IcontrolUnit IcontrolUnit;
+        public RiscProcessor(ControlUnit cu,ArithmeticLogicUnit alu)
+        {
+            registers = new Dictionary<string, short>
+            {
+                {"R0",0 }
+                ,{"R1",0}
+                ,{"R2",0}
+                ,{"R3",0}
+                ,{"R4",0}
+                ,{"R5",0}
+                ,{"R6",0}
+                ,{"R7",0}
+                ,{"SP",400}
+                ,{"LR",0}
+                ,{"PC",0}
+            };
+            instructions = new List<string>();
+            mainMemory = new List<short>(400);
+            cu.processor = this;
+            alu.processor = this;
+            cu.alu= alu;
+            IcontrolUnit = cu;
+            
+        }
+        public void load()
+        {
+            string path = "C:\\Users\\IO\\source\\repos\\FIC\\Program.txt";
+            instructions.Clear();
+            string[] preInstructions= File.ReadAllLines(path);
+            preInstructions = preInstructions.Where(x => x != string.Empty).ToArray();
+            //pre process part
+            for(int i=0;i<preInstructions.Length;i++)
+            {
+                string[] tokens = preInstructions[i].Split(' ');
+                if (!reservedKeywords.Contains(tokens[0]))
+                {
+                    labelLine.Add(tokens[0], (short)(i));
+                    preInstructions[i] = preInstructions[i].Trim().Substring(preInstructions[i].IndexOf(" ")+1);
+                    Console.WriteLine(preInstructions[i]);
+
+                }
+            }
+            instructions = preInstructions.ToList();
+        }
+        public void run()
+        {
+            while (instructions[registers["PC"]] != "HLT")
+            {
+                //dostuff
+                IcontrolUnit.Execute(instructions[registers["PC"]]);
+                registers["PC"]++;
+            }
+        }
+        public void StoreRegister(string reg,short value) {
+            registers[reg] = value;
+        }
+        public void PrintRegister(string reg) {
+            Console.WriteLine(registers[reg]);
+        }
+        public short getRegisterValue(string reg) { return registers[reg]; }
+        public short getLabelValue(string reg) { return labelLine[reg]; }
+    }
+}
